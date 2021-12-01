6512bb58b7ab4167d921664d3caaf49a6d4d04ea
Hamzammia <mahmudul.web.developer@gmail.com>
2021-12-01 18:44:17 +0600

    hjdf

---
+++ src/component/header.js
@@ -0,0 +1,18 @@
+import { Button } from 'bootstrap';
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <Button variant="primary">Primary</Button>{' '}
+            <Button variant="secondary">Secondary</Button>{' '}
+            <Button variant="success">Success</Button>{' '}
+            <Button variant="warning">Warning</Button>{' '}
+            <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+            <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+            <Button variant="link">Link</Button>
+        </div>
+    );
+};
+
+export default header;
